d246caf31a6cec991b4552f85bec7ef452de8cab
Phuongnhi23 <92927424+Phuongnhi23@users.noreply.github.com>
2021-10-21 20:05:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hè nhố =))!',
+    introDesc: `Bây giờ thì phải làm sao
+    Bây giờ ta phải làm sao bây giờ
+    Làm sao để biết bây giờ
+    Làm sao ta biết bây giờ làm sao ;-;`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'M là nhứt, nhứt m gòiii ;-; ',
+    desc: 'Có phải t chiều m quá, m hư phả không =))  ',
+    btnYes: 'Không t thấy m không điên tí nào😌 ',
+    btnNo: 'Không, m điên lắm ',
+    question:'Trên thế giới hơn 7 tỉ người mà chỉ có m khùng điên với t nhể '-'',
+    btnReply: 'Gửi m, thông cảm xíu ddeeeeee',
+    reply: 'Há há, đồ tồyyy, đồ khùng đồ đinnn ',
+    mess: 'Thôi t hiểu mà, riết rồi m cũng như t hoi =))',
+    messDesc: 'Má t cười ẻ. Thấy sự khùng điên của t chưa :)).',
+    btnAccept: 'Rồi nhá =))',
+    messLink: 'https://www.facebook.com/messages/t/100050725470311. VD: https://m.me/nam.nodemy
 }
